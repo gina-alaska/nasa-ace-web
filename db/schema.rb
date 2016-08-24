6fa524b6a986fe94b15d620429b06cb4bfa6d20a
Will Fisher <will@gina.alaska.edu>
2016-08-24 09:43:00 -0800

    initial loading of workspace idea

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160729010429) do
+ActiveRecord::Schema.define(version: 20160823190554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20160729010429) do
     t.string   "version"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "workspaces", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
