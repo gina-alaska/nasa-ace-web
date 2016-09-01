25d503803a411f59a28bd940282d9180e0f1340f
Will Fisher <will@gina.alaska.edu>
2016-09-01 14:30:43 -0800

    set the host correctly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-    config.action_cable.allowed_request_origins = /^nasa-ace.herokuapp.com$/
+  config.action_cable.allowed_request_origins = /(\.dev$)|^localhost$|(.herokuapp.com$)/
 end
