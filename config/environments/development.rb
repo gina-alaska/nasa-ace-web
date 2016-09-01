e35264871c39eaab10c2aeb68449c34162e6f81c
Will Fisher <will@gina.alaska.edu>
2016-09-01 14:26:41 -0800

    config to load heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,5 +52,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_cable.allowed_request_origins = /(\.dev$)|^localhost$/
+  config.action_cable.allowed_request_origins = /(\.dev$)|^localhost$|^nasa-ace.herokuapp.com$/
 end
