f0cfd3a169a5438bdabcb90d8a5af5c2d06ea3e1
Will Fisher <will@gina.alaska.edu>
2016-09-06 15:04:17 -0800

    adding layer category info

---
+++ app/views/categories/_category.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! category, :id, :name, :maptype, :postfix, :created_at, :updated_at
+json.url category_url(category, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827003423) do
+ActiveRecord::Schema.define(version: 20160906224353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "maptype"
+    t.string   "postfix"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "datasets", force: :cascade do |t|
     t.string   "title"
