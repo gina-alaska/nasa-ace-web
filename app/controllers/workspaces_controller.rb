8aa8d719ab41feb7db00240f41fe8ff8ff77f04d
grimm <jason@gina.alaska.edu>
2016-11-04 11:09:03 -0800

    Updated the workspace controller and views.

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -1,3 +1,65 @@
 # frozen_string_literal: true
 class WorkspacesController < ApplicationController
+  before_action :set_workspace, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @workspaces = Workspace.all
+  end
+
+  def show
+    redirect_to workspace_view_path(@workspace, @workspace.views.first.id)
+  end
+
+  def new
+    @workspace = Workspace.new
+  end
+
+  def edit
+  end
+
+  def create
+    @workspace = Workspace.new(workspace_params)
+
+    respond_to do |format|
+      if @workspace.save
+        format.html { redirect_to @workspace, notice: 'Workspace was successfully created.' }
+        format.json { render :show, status: :created, location: @workspace }
+      else
+        format.html { render :new }
+        format.json { render json: @workspace.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @workspace.update(workspace_params)
+        format.html { redirect_to @workspace, notice: 'Workspace was successfully updated.' }
+        format.json { render :show, status: :ok, location: @workspace }
+      else
+        format.html { render :edit }
+        format.json { render json: @workspace.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @workspace.destroy
+    respond_to do |format|
+      format.html { redirect_to workspaces_url, notice: 'Workspace was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_workspace
+    @workspace = Workspace.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def workspace_params
+    params.require(:workspace).permit(:name)
+  end
 end
