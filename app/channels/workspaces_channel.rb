36697de33e701faa4194593981bef565bca8b298
grimm <jason@gina.alaska.edu>
2016-11-07 14:22:22 -0900

    Fixed workspaces channel and js code.

--- app/channels/workspaces_channel.rb
+++ app/channels/workspaces_channel.rb
@@ -2,7 +2,7 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class WorkspacesChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "workspaces:workspace_#{current_workspace.id}"
+    stream_from "workspaces:#{channel_name}"
   end
 
   def unsubscribed
@@ -43,48 +43,56 @@ class WorkspacesChannel < ApplicationCable::Channel
 
   protected
 
+  def channel_name
+    "workspace_#{current_workspace.id}_#{current_view.id}"
+  end
+
   def rebroadcast(data)
     return if !current_workspace.presenter_id.blank? && current_workspace.presenter_id != params[:key]
-    WorkspacesChannel.broadcast_to("workspace_#{current_workspace.id}", data)
+    WorkspacesChannel.broadcast_to(channel_name, data)
   end
 
   def current_workspace
     Workspace.find(params[:id])
   end
 
+  def current_view
+    current_workspace.views.find(params[:view_id])
+  end
+
   def update_presenter(key)
-    if current_workspace.presenter_id.blank?
-      current_workspace.update_attributes(presenter_id: key)
+    if current_view.presenter_id.blank?
+      current_view.update_attributes(presenter_id: key)
     end
     presenter_notification
   end
 
   def clear_presenter(key)
-    if current_workspace.presenter_id == key
-      current_workspace.update_attributes(presenter_id: nil)
+    if current_view.presenter_id == key
+      current_view.update_attributes(presenter_id: nil)
     end
     presenter_notification
   end
 
   def presenter_notification
-    WorkspacesChannel.broadcast_to("workspace_#{current_workspace.id}", command: 'ws.presenter.update', id: current_workspace.presenter_id)
+    WorkspacesChannel.broadcast_to(channel_name, command: 'ws.presenter.update', id: current_view.presenter_id)
   end
 
   def update_layer_state(name, state = {})
-    layer = current_workspace.layers.where(name: name)
-    wl = current_workspace.workspace_layers.where(layer: layer).first
+    layer = current_view.layers.where(name: name)
+    wl = current_view.view_layers.where(layer: layer).first
     wl.update_attributes(state) unless wl.nil?
   end
 
   def update_workspace_state(state = {})
-    current_workspace.update_attributes(state)
+    current_view.update_attributes(state)
   end
 
   def reorder_layers(layers)
     layers.each_with_index do |name, index|
-      layer = current_workspace.layers.where(name: name).first
+      layer = current_view.layers.where(name: name).first
       unless layer.nil?
-        wl = current_workspace.workspace_layers.where(layer: layer).first
+        wl = current_view.view_layers.where(layer: layer).first
         wl.insert_at(index + 1)
       end
     end
