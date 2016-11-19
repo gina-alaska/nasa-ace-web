9eb06dc146cd2e9ad4b24976456fd7f7476570b3
grimm <jason@gina.alaska.edu>
2016-11-18 15:40:41 -0900

    Added test for view model duplicate method.

--- test/models/view_test.rb
+++ test/models/view_test.rb
@@ -2,12 +2,22 @@
 require 'test_helper'
 
 class ViewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @workspace = workspaces(:one)
+    @view = views(:one)
+  end
 
   test "to_s should return name" do
     view = View.new(name: "View Foo")
+
     assert_equal "View Foo", view.to_s
+  end
+
+  test "duplicate should return new view" do
+
+    assert_difference('@workspace.views.count') do
+      new_view = @view.duplicate(@workspace)
+      assert_equal "View1-duplicate", new_view.to_s
+    end
   end
 end
