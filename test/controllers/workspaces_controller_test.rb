b110de2a2f9787c86c78a61ea1c3e53ec28b151d
Jason Grimes <jason@gina.alaska.edu>
2016-11-21 15:56:09 -0900

    Create default workspace view (#34)
    * adding test for bug
    
    * New workspace now gets a default view
    
    Fixed test.

--- test/controllers/workspaces_controller_test.rb
+++ test/controllers/workspaces_controller_test.rb
@@ -35,6 +35,8 @@ class WorkspacesControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to workspace_url(Workspace.last)
+    follow_redirect!
+    assert_redirected_to workspace_view_path(Workspace.last, Workspace.last.views.first)
   end
 
   test "should get edit" do
