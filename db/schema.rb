58505b2a140c88003971b2abeed7aab1de6a9459
grimm <jason@gina.alaska.edu>
2017-02-02 15:59:57 -0900

    ran migration on test db.

--- db/schema.rb
+++ db/schema.rb
@@ -79,15 +79,6 @@ ActiveRecord::Schema.define(version: 20170125013924) do
     t.index ["workspace_id"], name: "index_workspace_layers_on_workspace_id", using: :btree
   end
 
-  create_table "workspace_views", force: :cascade do |t|
-    t.integer  "workspace_id"
-    t.integer  "view_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.index ["view_id"], name: "index_workspace_views_on_view_id", using: :btree
-    t.index ["workspace_id"], name: "index_workspace_views_on_workspace_id", using: :btree
-  end
-
   create_table "workspaces", force: :cascade do |t|
     t.string "name"
   end
