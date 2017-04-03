db9a227404a24e6b0afdb51e07c0a3615b152f80
Jason Grimes <jason@gina.alaska.edu>
2017-04-03 10:50:36 -0800

    Layer metadata display (#38)
    * Adding ckan_id to layer model.
    
    * Adding new ckan id.
    
    * Added ckan_id to layers controller, views
    
    * Added a layers helper method to get and display ckan metadata.
    
    * ran migration on test db.
    
    * update schema with just branch info
    
    * Moved layer metadata over to a popover with a button.
    
    * Moved code to new spot after merge.
    
    * No longer needed.
    
    * fixed schema

--- app/helpers/layers_helper.rb
+++ app/helpers/layers_helper.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 module LayersHelper
+  def parse_layer_api_metadata(layer)
+    params = { id: layer.ckan_id }
+    uri = URI("http://catalog.ace.uaf.edu/api/3/action/package_show")
+    uri.query = URI.encode_www_form(params)
+    metadata = JSON.parse(Net::HTTP.get_response(uri).body)
+    Rails.logger.info metadata["result"]['description']
+    metadata["result"]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170310233114) do
 
   # These are extensions that must be enabled in order to support this database
@@ -53,6 +52,7 @@ ActiveRecord::Schema.define(version: 20170310233114) do
     t.json     "style"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "ckan_id"
     t.index ["category_id"], name: "index_layers_on_category_id", using: :btree
   end
 
