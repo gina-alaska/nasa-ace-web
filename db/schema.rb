0dec363994fdf2c00f5194d17e585871f12466b2
grimm <jason@gina.alaska.edu>
2017-10-27 11:19:09 -0800

    Added user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310233114) do
+ActiveRecord::Schema.define(version: 20171027190354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,15 @@ ActiveRecord::Schema.define(version: 20170310233114) do
     t.datetime "updated_at",  null: false
     t.string   "ckan_id"
     t.index ["category_id"], name: "index_layers_on_category_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "ckan_api_key"
+    t.string   "name"
+    t.string   "group"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "view_layers", force: :cascade do |t|
