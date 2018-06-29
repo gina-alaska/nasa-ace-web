cdd3537c9cf1ed07abfbc271b6c21716f303d854
Keith Schneider <1900geek@gmail.com>
2018-06-29 14:55:14 -0500

    Updated layers_helper.rb so it works

--- app/helpers/layers_helper.rb
+++ app/helpers/layers_helper.rb
@@ -3,10 +3,16 @@
 module LayersHelper
   def parse_layer_api_metadata(layer)
     params = { id: layer.ckan_id }
-    uri = URI("http://catalog.ace.uaf.edu/api/3/action/package_show")
-    uri.query = URI.encode_www_form(params)
-    metadata = JSON.parse(Net::HTTP.get_response(uri).body)
-    Rails.logger.info metadata["result"]['description']
+    uri = URI.parse("http://ace.rdidev.com:8080/api/3/action/package_show?id=" + layer.ckan_id)
+    Rails.logger.info "Contacting uri"
+    req = Net::HTTP::Get.new(uri.to_s)
+    result = Net::HTTP.start(uri.host, uri.port ) {|http|
+        http.request(req)
+    }
+    #result = Net::HTTP.get("http://ace.rdidev.com/api/3/action/package_show?id=" + layer.ckan_id)
+    metadata = JSON.parse(result.body)
+    Rails.logger.info "metadata = #{metadata}"
+    # Rails.logger.info metadata["result"]['description']
     metadata["result"]
   end
 end
